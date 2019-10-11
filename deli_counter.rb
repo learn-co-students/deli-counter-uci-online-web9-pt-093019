f17412d0ce483d51e4d9a3432af4c8c45343c02c
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 02:49:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+def take_a_number(deli, name)
+  deli << name 
+  position = deli.index(name) + 1 
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+
+
+
+
+def line(deli)
+  # if line is empty return empty message and return from method 
+if deli == [] 
+  puts "The line is currently empty."
+  return 
+end
+#if line is not empty create a line message then print that message 
+  message = "The line is currently:"
+  position = 1 
+  index = 0 
+  while index < deli.length  do 
+    name = deli[index]
+    message << " #{position}. #{name}"
+    position += 1 
+    index += 1 
+end
+
+puts message 
+end
+
+
+
+
+
+def now_serving(deli)
+  if deli == []
+    puts "There is nobody waiting to be served!"
+    return 
+  end
+  
+  person = deli.shift 
+  puts "Currently serving #{person}."
+  
+end
